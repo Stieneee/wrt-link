ccc6bcb14df84c260b31be12e7f8accf47915283
Tyler Stiene <tystiene@gmail.com>
2019-03-06 02:26:25 -0500

    init file fixes

--- init-wrt-link.sh
+++ init-wrt-link.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-echo 'init-wrt-link.sh version 0.2.1' >> /tmp/wrt-link.log
+echo 'init-wrt-link.sh version 0.2.2' >> /tmp/wrt-link.log
 
 cpuinfo=$(cat /proc/cpuinfo)
 
-if [[ $cpuinfo == *"syscall"* ]]; then
+if [[ $cpuinfo == *"mips"* ]]; then
   echo 'MIPS CPU Detected' >> /tmp/wrt-link.log
   until scp -P 222 public@get.logmy.io:latest/wrt-link-mips /tmp/wrt-link; do 
-    echo "Failed to download init-wrt-link.log. trying again in 5 second" >> wrt-link.log
+    echo "Failed to download wrt-link-mips. trying again in 5 second" >> wrt-link.log
     sleep 5 
   done
 else
@@ -16,8 +16,6 @@ else
 fi
 
 chmod u+x /tmp/wrt-link
-
-/tmp/wrt-link
 
 _stop() {
   test -f /tmp/stop-wrt-link && echo "Stopping!" && rm /tmp/stop-wrt-link && exit 0 || return 0
